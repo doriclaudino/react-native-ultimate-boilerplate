a7e575fea169d316255ebc43f147783b2a559eec
doriclaudino <dori.claudino@gmail.com>
2019-03-30 12:26:43 -0400

    react-native eject files after change name

--- ios/react-native-ultimate-boilerplateTests/react-native-ultimate-boilerplateTests.m
+++ ios/react-native-ultimate-boilerplateTests/react-native-ultimate-boilerplateTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface react_native_ultimate_boilerplateTests : XCTestCase
+@interface react-native-ultimate-boilerplateTests : XCTestCase
 
 @end
 
-@implementation react_native_ultimate_boilerplateTests
+@implementation react-native-ultimate-boilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
