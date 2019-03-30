7b82ab6f411728e0c41951e2851a1a5f42144166
doriclaudino <dori.claudino@gmail.com>
2019-03-30 14:04:26 -0400

    native storybook inside app

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
+import StorybookUI from './storybook';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const enable_storybook = true
+const registerApp = enable_storybook ? StorybookUI : App
+AppRegistry.registerComponent(appName, () => registerApp);
